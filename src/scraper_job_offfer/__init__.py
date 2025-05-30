e95400867431debc12d8c00144924f5a98dff434
Chuwi12 <srpc9682@protonmail.com>
2025-05-30 19:43:45 +0200

    Codígo  para extraer el titulo, el nivel, si es remoto y las tecnologías

--- src/scraper_job_offfer/__init__.py
+++ src/scraper_job_offfer/__init__.py
@@ -6,16 +6,54 @@ import csv
 def extrar_ofertas(url):
 
     # Ingreso de petición http get en la variable, simulando que soy un navegador Mozilla
-    res = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    req = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 
     # Comprobación de estado 
-    if res.status_code == 200:
+    if req.status_code == 200:
         
         # Parseo de la página web
-        soup = BeautifulSoup(res.txt, "html.parser")
+        soup = BeautifulSoup(req.txt, "html.parser")
+
+        # Lista de las ofertas de trabajo
+        ofertas = []
+
+        # Iterar las ofertas de trabajo
+        for oferta in soup.find_all("artible"):
+
+            # Extracción de nombre de la oferta de trabajo
+            titulo_elemento = oferta.find("h2")
+
+            # Comprobación de la existencía del titulo, sino esxiste se asigana none
+            titulo = titulo_elemento.get_text(strip=True) if titulo_elemento else "None"
+
+            # Comprobación de nivel de experiencía de la oferta 
+            # En mi caso de junior y entry modificable para futuros usos
+            nivel_elemento = oferta.find("span", class_="experience-lever")
+
+            # Comprobación de la existencia del nivel de la oferta
+            nivel = nivel_elemento.get_text(strip=True).lower() if nivel_elemento else "No se especifica"
+
+            # Comprobación de que la oferta sea para junior o entry
+            if "junior"  not in nivel and "entry" not in nivel:
+                continue
+            
+            # Comprobación de si la oferta es en remoto, exactamente el mismo procedimeinto del anterior
+            ubicacion_elemento = oferta.find("span", class_="location")
+            ubicacion = ubicacion_elemento.get_text(strip=True).lower() if ubicacion_element else "No es remota"
+            if "remote" not in ubicacion:
+                continue
+
+            # Extración de salarío si este se encuentra
+            salario_elemento = oferta.find("span", class_="salary")
+            salario = salario_elemento.get_text(strip=True) if salario_element else "No especificado"
+
+            # Obtener las tecnologías que piden
+            tecnologias = []
+
+
 
     else: 
-        print("El codigo de erro es: " + res.status_code)
+        print("El codigo de erro es: " + req.status_code)
         
 
 
