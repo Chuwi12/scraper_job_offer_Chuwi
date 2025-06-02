dbd8c1a9844c5b98bc2c6f2dab4a548b0a502441
Chuwi12 <srpc9682@protonmail.com>
2025-06-02 22:43:14 +0200

    Delete test

--- src/scraper_job_offfer/__init__.py
+++ src/scraper_job_offfer/__init__.py
@@ -12,13 +12,13 @@ def extrar_ofertas(url, nombre_archivo):
     if req.status_code == 200:
         
         # Parseo de la página web
-        soup = BeautifulSoup(req.txt, "html.parser")
+        soup = BeautifulSoup(req.text, "html.parser")
 
         # Lista de las ofertas de trabajo
         ofertas = []
 
         # Iterar las ofertas de trabajo
-        for oferta in soup.find_all("artible"):
+        for oferta in soup.find_all("article"):
 
             # Extracción de nombre de la oferta de trabajo
             titulo_elemento = oferta.find("h2")
@@ -28,7 +28,7 @@ def extrar_ofertas(url, nombre_archivo):
 
             # Comprobación de nivel de experiencía de la oferta 
             # En mi caso de junior y entry modificable para futuros usos
-            nivel_elemento = oferta.find("span", class_="experience-lever")
+            nivel_elemento = oferta.find("span", class_="experience-level")
 
             # Comprobación de la existencia del nivel de la oferta
             nivel = nivel_elemento.get_text(strip=True).lower() if nivel_elemento else "No se especifica"
@@ -39,13 +39,13 @@ def extrar_ofertas(url, nombre_archivo):
             
             # Comprobación de si la oferta es en remoto, exactamente el mismo procedimeinto del anterior
             ubicacion_elemento = oferta.find("span", class_="location")
-            ubicacion = ubicacion_elemento.get_text(strip=True).lower() if ubicacion_element else "No es remota"
+            ubicacion = ubicacion_elemento.get_text(strip=True).lower() if ubicacion_elemento else "No es remota"
             if "remote" not in ubicacion:
                 continue
 
             # Extración de salarío si este se encuentra
             salario_elemento = oferta.find("span", class_="salary")
-            salario = salario_elemento.get_text(strip=True) if salario_element else "No especificado"
+            salario = salario_elemento.get_text(strip=True) if salario_elemento else "No especificado"
 
             # Obtener las tecnologías que piden
             tecnologias = []
@@ -74,10 +74,10 @@ def extrar_ofertas(url, nombre_archivo):
 
 
 # Ingreso de url
-url = input("Ingresa la url que quieras hacer el scaneo:")
+url = input("Ingresa la url que quieras hacer el scaneo:\n")
 
 # Ingreso de nombre del archivo
-nombre_archivo = input("Nombre del archivo:")
+nombre_archivo = input("Nombre del archivo:\n")
 
 # Llamada a la función
 ofertas = extrar_ofertas(url, nombre_archivo)
